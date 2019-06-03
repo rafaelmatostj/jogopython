b2f88cce7ceb67447ed9d5c268cb7e16f0bec8dc
rafaelmatostj <rafaelmatostj@gmail.com>
2019-06-03 16:11:34 -0300

    teste01

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
   def __init__(self, *filhos, nome=None, idade=35):
       self.idade = idade
       self.nome = nome
@@ -6,9 +7,21 @@ class Pessoa:
   def cumprimentar(self):
       return f'ola{id(self)}'
 
+  @staticmethod
+  def metodo_estatico():
+      return 42
+
+  @classmethod
+  def nome_e_atributos_de_classe(cls):
+      return f'{cls} - olhos {cls.olhos}'
+
+class Homem(Pessoa):
+    pass
+
+
 if __name__ == '__main__':
-    rafael = Pessoa(nome='Rafael')
-    luciano = Pessoa(rafael, nome='Rafael')
+    rafael = Homem(nome='Rafael')
+    luciano = Homem(rafael, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -17,5 +30,16 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sobrenome = 'alves'
     del luciano.filhos
+    luciano.olhos = 1
+    del luciano.olhos
     print(luciano.__dict__)
     print(rafael.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(rafael.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(rafael.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metado_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.metodo_estatico())
+    pessoa=Pessoa('anonimo')
+    print(isis)
